75035c621b1a725b457b415fd79b89f4d016866d
ledinhhieu22122002 <ledinhhieu1212003@gmail.com>
2022-06-28 18:02:42 +0700

    up load

--- project/pofolio/assets/js/main.js
+++ project/pofolio/assets/js/main.js
@@ -1,28 +1,31 @@
-// hide show menu 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    });
-});
+
+
+window.onscroll = () => { showHeader() };
+const showHeader = () => {
+  const header = document.querySelector('header');
+  const slide = document.querySelector('.slide');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    slide.style.transform = 'translateY(0)';
+    header.classList.add('sticky');
+  } else {
+    slide.style.transform = 'translateY(100px)';
+    header.classList.remove('sticky');
+  }
+};
 
 // MENU BUTTON
-const menu = document.querySelector(".menu-pc");
-const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector("ul");
+const menuButton = document.querySelector(".bart-btn");
 const overlay = document.querySelector("#overlay");
 
-menuButton.addEventListener("click", () => {
+menuButton.onclick = () => {
   menu.classList.toggle("navbar-open");
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
-});
+};
 
-overlay.addEventListener("click", () => {
+overlay.onclick = () => {
   menu.classList.toggle("navbar-open");
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
-});
+};
